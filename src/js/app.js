f18075c47020352b7f99d8406856771c13a866f1
MyCode07 <erem19992015@mail.ru>
2024-01-16 18:25:18 +0300

    add new contact page

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,7 @@ import "./parts/offers.js";
 import { stickyHeader } from "./parts/header.js";
 import { calculator } from "./parts/calculator.js";
 import { playVideoAction } from "./parts/video.js";
+import { createMap } from "./parts/map.js";
 
 playVideoAction();
 aniamteTrigger();
@@ -24,6 +25,7 @@ maskInputs('+7 (999) 999-99-99', '._mask-phone')
 replaceDomElements();
 stickyHeader();
 calculator.start();
+createMap();
 
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
